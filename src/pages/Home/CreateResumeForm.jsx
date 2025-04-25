6ccabb4a2bd89faf39fb2c4a73c145d362bd4185
omarbinsaleh <omarbinsaleh44@gmail.com>
2025-04-25 15:03:24 +0400

    initial commite: setup the project and installed ncessary packages

---
+++ src/pages/Home/CreateResumeForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateResumeForm = () => {
+  return (
+    <div>
+      Create Resume Form
+    </div>
+  )
+}
+
+export default CreateResumeForm
---
+++ src/pages/ResumeUpdate/EditResume.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditResume = () => {
+  return (
+    <div>
+      Edit Resume page
+    </div>
+  )
+}
+
+export default EditResume
